f553cfdcac9a0b164380eb9073801c29ebebad6b
liuyan <lyfx99@icloud.com>
2024-10-28 16:56:42 +0800

    feat: 修改smina的init

--- biorange/dock/__init__.py
+++ biorange/dock/__init__.py
@@ -19,10 +19,3 @@ from biorange.dock.dock_plot_interaction import (
 
 # 评分
 from biorange.dock.dock_score import smina_score
-
-if __name__ == "__main__":
-    x = smina_score(
-        ligand_file="results/complex_0/rank1_confidence-0.77.sdf",
-        receptor_file="results/6w70.pdb",
-    )
-    print(x)
